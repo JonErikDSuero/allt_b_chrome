6d0e39915b3b6ff51474120f36184404cf63c9ff
Jon Erik Suero <jonerikdsuero@gmail.com>
2014-06-30 22:50:26 -0400

    change icons if bookmarked

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "bookmark_through_keyboard") {
-    bookmark("through_keyboard"); // pass an arbitrary value
+    detectCurrentTab(); // pass an arbitrary value
   }
 });
 
@@ -8,53 +8,73 @@ chrome.commands.onCommand.addListener(function(command) {
 chrome.contextMenus.create({
   title: "Alt B",
   contexts:["all"],
-  onclick: bookmark
+  onclick: detectCurrentTab
 });
 
 
-function bookmark(info) {
+chrome.tabs.onActivated.addListener(function(activeInfo) { //listen for new tab to be activated
+  detectCurrentTab("do_not_bookmark");
+});
 
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { //listen for current tab to be changed
+  detectCurrentTab("do_not_bookmark");
+});
+
+
+function toggleBookmarkedIcon(bookmarked) {
+  var icon_path = ((bookmarked) ? 'heart_icon.png' : 'icon.png');
+  chrome.browserAction.setIcon({path: icon_path});
+}
+
+
+function detectCurrentTab(info) {
   chrome.tabs.getSelected(null, function(tab) {
-
     chrome.bookmarks.search({url: tab.url}, function(similar_bookmarks){
+      var current_tab_is_bookmarked = (similar_bookmarks.length != 0);
 
-      if (similar_bookmarks.length == 0) { // if the current page is not yet bookmarked, allow bookmarking
-        var tag_raw = ((info.selectionText == undefined) ? tab.title : info.selectionText);
-        tag_raw = tag_raw.toLowerCase().replace(/[^A-Za-z ]/g,'');
-        var url = tab.url;
-
-        chrome.bookmarks.getTree( function(bookmarks_tree_root) {
-          var children_of_bookmarks_bar = bookmarks_tree_root[0].children[0];
-          var folders = [];
-          var folders = get_folders(children_of_bookmarks_bar, folders);
-          var best_folder = get_best_folder(folders, tag_raw);
-          
-          chrome.tabs.executeScript(tab.id, {code: "var folder_title = '"+best_folder.title+"';"}, function() {
-            chrome.tabs.executeScript(tab.id, {file: 'jquery-2.1.1.min.js'}, function() {
-              chrome.tabs.executeScript(tab.id, {file: 'bookmarked_modal.js'});
-            });
-          });
-
-          chrome.bookmarks.create({
-            'parentId': best_folder.id,
-            title: tab.title,
-            url: url
-          });
-
-        });
+      if (info == "do_not_bookmark") {
+        toggleBookmarkedIcon(current_tab_is_bookmarked);
+      } else {
+        if (!current_tab_is_bookmarked) {
+          toggleBookmarkedIcon(true);
+          bookmark(tab, info);
+        }
       }
     });
   });
 }
 
 
-function get_folders(tree_node, folders) {
+function bookmark(tab, info) {
+  var url = tab.url;
+  var tag_raw = (((info == undefined) || (info.selectionText == undefined)) ? tab.title : info.selectionText);
+  tag_raw = tag_raw.toLowerCase().replace(/[^A-Za-z ]/g,'');
+
+  chrome.bookmarks.getTree( function(bookmarks_tree_root) {
+    var children_of_bookmarks_bar = bookmarks_tree_root[0].children[0];
+    var folders = [];
+    var folders = getFolders(children_of_bookmarks_bar, folders);
+    var best_folder = getBestFolder(folders, tag_raw);
+    
+    chrome.tabs.executeScript(tab.id, {code: "var folder_title = '"+best_folder.title+"';"}, function() {
+      chrome.tabs.executeScript(tab.id, {file: 'jquery-2.1.1.min.js'}, function() {
+        chrome.tabs.executeScript(tab.id, {file: 'bookmarked_modal.js'});
+      });
+    });
+
+    chrome.bookmarks.create( { 'parentId': best_folder.id, title: tab.title, url: url } );
+  });
+}
+
+
+function getFolders(tree_node, folders) {
   if (tree_node.children != undefined) { // if it is a folder
     var i;
     var children = tree_node.children;
 
     for (i = 0; i < children.length; i++) {
-      get_folders(children[i], folders);
+      getFolders(children[i], folders);
     }
 
     folders.push(tree_node);
@@ -63,7 +83,7 @@ function get_folders(tree_node, folders) {
 }
 
 
-function get_best_folder(folders, tag_raw) { // TODO: improve by adding back-end server?
+function getBestFolder(folders, tag_raw) { // TODO: improve by adding back-end server?
   var i;
   for (i = 0; i < folders.length; i++) {
     folder_title = folders[i].title.toLowerCase().replace(/[^A-Za-z ]/g,'');
